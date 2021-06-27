b7ef67ed7204c6162a0dad0a09d4794bdd41e5e9
syoukera <s.akira2986@gmail.com>
2021-06-27 16:51:06 +0900

    Delete old codes

--- boid_py/core.py
+++ boid_py/core.py
@@ -10,8 +10,8 @@ class boid:
 		# attributes:
 		self.N = num_boid
 		# strength of force
-		self.cohesion_force = 0.0008
-		self.separation_force = 0.15
+		self.cohesion_force = 0.08
+		self.separation_force = 1.15
 		self.alignment_force = 1.0
 
 		# distance at force-induction
@@ -124,39 +124,3 @@ class boid:
 		self.v[max_vel_bool] = self.max_vel*np.divide(self.v[max_vel_bool].T, v_abs[max_vel_bool]).T
 
 		self.x += self.v
-		
-		# for i in range(self.N):
-		# 	x_this = self.x[i]
-		# 	v_this = self.v[i]
-			
-			# x_that = np.delete(self.x, i, axis=0)
-			# v_that = np.delete(self.v, i, axis=0)
-			# x_that = self.x
-			# v_that = self.v
-
-			# self.diff_x[i] = x_that - x_this
-			
-			# self.distance[i] = np.linalg.norm(self.diff_x[i], axis=1)
-			# self.angle[i] = np.arccos(np.dot(v_this, (self.diff_x[i]).T) / (np.linalg.norm(v_this) * \
-			# 	    np.linalg.norm((self.diff_x[i]), axis=1)))
-			
-			# coh_agents_x = x_that[ (self.distance[i] < self.cohesion_distance) &   (self.angle[i] < self.cohesion_angle)]
-			# sep_agents_x = x_that[ (self.distance[i] < self.separation_distance) & (self.angle[i] < self.separation_angle)]
-			# ali_agents_v = v_that[ (self.distance[i] < self.alignment_distance) &  (self.angle[i] < self.alignment_angle)]
-
-			# self.dv_coh[i] = self.cohesion_force * (np.average(coh_agents_x, axis=0) - x_this) if (len(coh_agents_x) > 0) else 0
-			# self.dv_sep[i] = self.separation_force * (np.average(sep_agents_x, axis=0) - x_this) if (len(sep_agents_x) > 0) else 0
-			# self.dv_ali[i] = self.alignment_force * (np.average(ali_agents_v, axis=0) - v_this) if (len(ali_agents_v) > 0) else 0
-			
-			# dist_center = np.linalg.norm(x_this)
-			# self.dv_boundary[i] = - self.boundary_force * x_this * (dist_center - 1) / dist_center if (dist_center > 1) else 0
-
-		# self.v += self.dv_coh + self.dv_sep + self.dv_ali + self.dv_boundary
-		# for i in range(self.N):
-		# 	v_abs = np.linalg.norm(self.v[i])
-		# 	if (v_abs < self.min_vel):
-		# 		self.v[i] = self.min_vel * self.v[i] / v_abs
-		# 	elif (v_abs > self.max_vel):
-		# 		self.v[i] = self.max_vel * self.v[i] / v_abs
-
-		# self.x += self.v
